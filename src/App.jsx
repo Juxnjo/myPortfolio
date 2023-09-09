99af7c12efd2aed5e81d8a502124bb118b8c4b64
“Juan <ramirezjuanj0117@gmail.com>
2023-09-09 10:27:56 -0500

    Me component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { NavBar } from "./components/navbar/NavBar";
+import { Me } from "./components/Me/Me";
 
-export default function App() {
+export default function App ()
+{
   return (
     <div className="bg-primary">
-      <NavBar/>
+      <NavBar />
+      <Me />
     </div>
   )
 }
